511073bc1c8b4fd8005e5cf5ee7016ce3a325951
1RafaelLacerda <rafael.lr00@outlook.com>
2020-12-05 08:16:31 -0300

    enviando questão 4

---
+++ Atividades/Atividade04/Atividade 4/Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class Player : MonoBehaviour 
+{
+    
+    public float Movespeed = 3.5f;
+    public float Turnspeed = 120f;
+
+    
+    void Update()
+    {
+
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+        this.transform.Translate(Vector3.forward * vert * Movespeed * Time.deltaTime);
+        this.transform.localRotation *= Quaternion.AngleAxis(horz * Turnspeed * Time.deltaTime, Vector3.up);
+    }
+}
---
+++ Atividades/Atividade04/Atividade 4/Assets/PlayerSight.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSight : MonoBehaviour
+{
+    [Header("GameObjects")]
+    private GameObject arma;
+    public GameObject bullet;
+    public Transform canoArma;
+    [Header("Valores Pistola")]
+    public int municao;
+    public int pente;
+    private float fireHate;
+    private float timeToShoot;
+    [Header("Audio e Animações")]
+    public AudioSource pistolaSFX;
+    public AudioSource ReloadWeapon;
+    void Awake()
+    {
+        municao = 1000000;
+        pente = 3;
+        fireHate = 1.5f;
+        timeToShoot = 3;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (municao > 0)
+            {
+                if (timeToShoot > 0.2)
+                {
+                    municao--;
+                    //pistolaSFX.Play();
+                    Instantiate(bullet, canoArma.position, canoArma.rotation);
+                    timeToShoot = 0;
+                }
+            }
+        }
+        timeToShoot += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (municao == 0 && pente > 0)
+            {
+                pente -= 1;
+                municao = 10;
+            }
+        }
+    }
+
+}
